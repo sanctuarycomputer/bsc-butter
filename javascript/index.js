d9aa2ad9ee576b9e468f13898bb9440d0a25d6f9
Jenna Rothstein <rothstein.jenna@gmail.com>
2017-03-22 18:01:28 -0400

    Slidey function/arrows/blue box/titles

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,41 @@ $(document).ready(function (){
     dots: false,
     infinite: false,
     speed: 300,
-    arrows: false,
+    slidesToShow: 1,
+    nextArrow: $('#triangle-right'),
+    prevArrow: $('#triangle-left')
+  });
+  $('#triangle-left').css("display","none");
+  $('.slick-slider-content').on('afterChange', function(){
+    let currentSlide = $('.slick-slider-content').slick('slickCurrentSlide');
+    let index = $('.slick-slide').length;
 
-    slidesToShow: 1
+    if (currentSlide === 0){
+      $('#triangle-left').css("display","none");
+    }else{
+      $('#triangle-left').css("display","block");
+    }
+    if ((currentSlide + 1 )=== index){
+      $('#triangle-right').css("display","none");
+    }else{
+      $('#triangle-right').css("display","block");
+    }
+
+    let titles = document.querySelectorAll('.steps-information-title ');
+
+    titles.forEach((title) => {
+      title.style.display = 'none';
     });
+
+    titles[currentSlide].style.display = 'inline-block';
+    // for(var i = 0; i <title.length; i++) {
+    //   let currentTitle = title[i];
+    //   if(currentTitle.dataset.index === (currentSlide+1)){
+    //     currentTitle.style.opacity = "1";
+    //   }else{
+    //     currentTitle.style.opacity = "0";
+    //   }
+    //   console.log(currentTitle, (currentSlide+1))
+    // }
+  });
 });
