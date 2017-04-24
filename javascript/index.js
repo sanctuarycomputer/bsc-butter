f5026566e6e289a0c99703be07f3aa9342960c0a
Hugh Francis <me@hughfrancis.me>
2017-04-23 20:32:27 -0400

    Changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,63 +1,17 @@
 $(document).ready(function (){
-
-  $('.slick-slider-content').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    nextArrow: $('#triangle-right'),
-    prevArrow: $('#triangle-left')
-  });
-
-  $('.slick-slider-content').on('afterChange', function(){
-    let $currentSlide = $('.slick-active')
-    let currentSlideMethod = $currentSlide.attr('data-method')
-    let $titles = $('.steps-information-title')
-    let $slidingBarTitles = $('.middle-title')
-    $titles.each((index, title) => {
-      let $title = $(title);
-      if ($title.attr('data-method') === currentSlideMethod) {
-        $title.addClass('active');
+  $('.trigger').waypoint({
+    offset: 1,
+    handler(direction) {
+      if (direction === 'down') {
+        $('nav').addClass('active');
       } else {
-        $title.removeClass('active');
+        $('nav').removeClass('active');
       }
-    });
-    /*
-    $slidingBarTitles.each((index, slidingBarTitle) => {
-      let $slidingBarTitle = $(slidingBarTitle);
-      if ($slidingBarTitle.attr('data-method') === currentSlideMethod) {
-        animation2();
-        $slidingBarTitle.addClass('active')
-      } else{
-        animation1();
-        $slidingBarTitle.removeClass('active')
-      }
-    });
-    */
+    }
   });
-
-  /*
-  var el = document.getElementById("sliding-underline")
-  function animation1(){
-    dynamics.animate(el, {
-      translateX: -100,
-    }, {
-      type: dynamics.spring,
-      frequency: 100,
-      friction: 200,
-      duration: 1500,
-    })
-  }
-  animation1();
-  function animation2(){
-    dynamics.animate(el, {
-      translateX: 100,
-    }, {
-      type: dynamics.spring,
-      frequency: 100,
-      friction: 200,
-      duration: 1500,
-    })
-  }
-  */
+  $('.prompt').on('click', function() {
+    $.smoothScroll({
+      scrollTarget: $('.prompt')
+    });
+  });
 });
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -2,67 +2,21 @@
 'use strict';
 
 $(document).ready(function () {
-
-  $('.slick-slider-content').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    nextArrow: $('#triangle-right'),
-    prevArrow: $('#triangle-left')
-  });
-
-  $('.slick-slider-content').on('afterChange', function () {
-    var $currentSlide = $('.slick-active');
-    var currentSlideMethod = $currentSlide.attr('data-method');
-    var $titles = $('.steps-information-title');
-    var $slidingBarTitles = $('.middle-title');
-    $titles.each(function (index, title) {
-      var $title = $(title);
-      if ($title.attr('data-method') === currentSlideMethod) {
-        $title.addClass('active');
+  $('.trigger').waypoint({
+    offset: 1,
+    handler: function handler(direction) {
+      if (direction === 'down') {
+        $('nav').addClass('active');
       } else {
-        $title.removeClass('active');
+        $('nav').removeClass('active');
       }
-    });
-    /*
-    $slidingBarTitles.each((index, slidingBarTitle) => {
-      let $slidingBarTitle = $(slidingBarTitle);
-      if ($slidingBarTitle.attr('data-method') === currentSlideMethod) {
-        animation2();
-        $slidingBarTitle.addClass('active')
-      } else{
-        animation1();
-        $slidingBarTitle.removeClass('active')
-      }
-    });
-    */
+    }
   });
-
-  /*
-  var el = document.getElementById("sliding-underline")
-  function animation1(){
-    dynamics.animate(el, {
-      translateX: -100,
-    }, {
-      type: dynamics.spring,
-      frequency: 100,
-      friction: 200,
-      duration: 1500,
-    })
-  }
-  animation1();
-  function animation2(){
-    dynamics.animate(el, {
-      translateX: 100,
-    }, {
-      type: dynamics.spring,
-      frequency: 100,
-      friction: 200,
-      duration: 1500,
-    })
-  }
-  */
+  $('.prompt').on('click', function () {
+    $.smoothScroll({
+      scrollTarget: $('.prompt')
+    });
+  });
 });
 
 },{}]},{},[1]);
